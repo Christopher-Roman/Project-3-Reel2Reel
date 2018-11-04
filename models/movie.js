e456e326dc026097afbce012d239cb49917e59aa
Chris Roman <Christopherroman85@gmail.com>
2018-11-04 13:54:38 -0600

    started writing routes for the backend

--- models/movie.js
+++ models/movie.js
@@ -1,9 +1,24 @@
 const movieSchema = new mongoose.Schema({
-    title: String,
-    genre: String,
-    runtime: Number,
-    director: String,
-    img: String
+    title: {
+    	type: String,
+    	required: true,
+    },
+    genre: {
+    	type: String,
+    	required: true,
+    },
+    runtime: {
+    	type: Number,
+    	required: true,
+    },
+    director: {
+    	type: String,
+    	required: true,
+    },
+    img: {
+    	type: String,
+    	required: true,
+    },
 })
 
 module.exports = mongoose.model('Movie', movieSchema);
--- models/user.js
+++ models/user.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 const movieSchema = require('./movieSchema');
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    watchList: [movieSchema],
-    favMovies: [movieSchema],
-    ownedMovies: [movieSchema]
+    name: {
+    	type: String,
+    	required: true
+    },
+    watchList: [Movie.schema],
+    favMovies: [Movie.schema],
+    ownedMovies: [Movie.schema]
 })
 
 module.exports = mongoose.model('User', userSchema);
